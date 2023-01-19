116305b85473d1e5a5064bf77659e352a4bde84d
Clément <clement@cantoo.co>
2023-01-19 16:59:02 +0100

    feat:

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <h1 v-show="toggle">Bonjour</h1>
-  <button @click="toggle = !toggle">Cacher</button>
+  <ul>
+    <li v-for="user of utilisateurs" :key="user.prenom">
+      {{
+        `${user.prenom} : ${Math.round(
+          user.notes.reduce((a, b) => a + b) / user.notes.length
+        )}`
+      }}
+    </li>
+  </ul>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue";
 
-const toggle = ref(true);
+<script setup lang="ts">
+import { reactive } from "vue";
+const utilisateurs = reactive([
+  { prenom: "Jean", notes: [14, 15, 18] },
+  { prenom: "Paul", notes: [12, 10, 8] },
+  { prenom: "Pierre", notes: [3, 15, 18] },
+  { prenom: "Bob", notes: [12, 15, 18] },
+  { prenom: "Julie", notes: [12, 15, 18] },
+  { prenom: "Lucie", notes: [12, 20, 18] },
+  { prenom: "Paulette", notes: [19, 13, 18] },
+]);
 </script>
 
-<style scoped lang="scss"></style>
+
+<style>
+</style>
