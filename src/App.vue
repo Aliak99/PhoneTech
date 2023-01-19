ed1dcc7d8e5d2d72e5176d51e3ff6bda12579f5d
Clément <clement@cantoo.co>
2023-01-19 16:30:13 +0100

    feat:

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 v-show="toggle">Bonjour</h1>
+  <button @click="toggle = !toggle">Cacher</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const toggle = ref(true);
+</script>
+
+<style scoped lang="scss"></style>
