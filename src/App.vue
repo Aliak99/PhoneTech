40a23b010a1a2d3c891bc19df5e1558d96fb93a5
Clément <clement@cantoo.co>
2023-01-20 12:31:13 +0100

    feat(Ex-Les directives structurelles): Mini-projet de liste de tâches

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
-  <ul>
-    <li v-for="user of utilisateurs" :key="user.prenom">
-      {{
-        `${user.prenom} : ${Math.round(
-          user.notes.reduce((a, b) => a + b) / user.notes.length
-        )}`
-      }}
-    </li>
+  <form @submit.prevent="addTodo()">
+    <label for="add-todo">Ajouter une Todo</label>
+    <input v-model="tache" id="add-todo" type="text" />
+    <button>Ajouter</button>
+  </form>
+  <ul v-if="todos.length">
+    <template v-for="(todo, index) in todos" :key="todo.id">
+      <li>
+        {{ todo.titre }}
+
+        <button @click="suppTodo(index)">Supprimer</button>
+      </li></template
+    >
   </ul>
 </template>
 
-
 <script setup lang="ts">
-import { reactive } from "vue";
-const utilisateurs = reactive([
-  { prenom: "Jean", notes: [14, 15, 18] },
-  { prenom: "Paul", notes: [12, 10, 8] },
-  { prenom: "Pierre", notes: [3, 15, 18] },
-  { prenom: "Bob", notes: [12, 15, 18] },
-  { prenom: "Julie", notes: [12, 15, 18] },
-  { prenom: "Lucie", notes: [12, 20, 18] },
-  { prenom: "Paulette", notes: [19, 13, 18] },
-]);
+import { ref } from "vue";
+
+const tache = ref("");
+const todos = ref<{ id: Number; titre: String }[]>([]);
+
+function addTodo() {
+  todos.value.push({ id: Date.now(), titre: tache.value });
+  tache.value = "";
+}
+
+function suppTodo(index: number) {
+  todos.value.splice(index, 1);
+}
 </script>
-
-
-<style>
-</style>
