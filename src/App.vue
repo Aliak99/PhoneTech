dda0988d839eb9b0b3187918f915e5af4530f7d8
Clément <clement@cantoo.co>
2023-01-20 18:09:37 +0100

    feat(Les bases des composants): :fire: Le cycle de vie d'un composant

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
-  <form @submit.prevent="addTodo()">
-    <label for="add-todo">Ajouter une Todo</label>
-    <input v-model="tache" id="add-todo" type="text" />
-    <button>Ajouter</button>
-  </form>
-  <ul v-if="todos.length">
-    <template v-for="(todo, index) in todos" :key="todo.id">
-      <li>
-        {{ todo.titre }}
-
-        <button @click="suppTodo(index)">Supprimer</button>
-      </li></template
-    >
-  </ul>
+  <button @click="toggle = !toggle">Toggle</button>
+  <div v-if="toggle">
+    <Blog />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import Blog from "./Blog.vue";
 
-const tache = ref("");
-const todos = ref<{ id: Number; titre: String }[]>([]);
-
-function addTodo() {
-  todos.value.push({ id: Date.now(), titre: tache.value });
-  tache.value = "";
-}
-
-function suppTodo(index: number) {
-  todos.value.splice(index, 1);
-}
+const toggle = ref(true);
 </script>
+
+<style scoped lang="scss"></style>
