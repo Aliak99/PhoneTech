00babae8cd1869a0db737aff7184ba278dd34ade
Clément <clement@cantoo.co>
2023-02-09 16:00:36 +0100

    feat(chapitre9): :fire: Provide et Inject

---
+++ src/Author.vue
@@ -0,0 +1,24 @@
+<template>
+  <small> {{ author.name }} - {{ author.birthdate }}</small>
+  <p>{{ api }}</p>
+</template>
+
+<script setup lang="ts">
+import { inject } from "vue";
+import type { AuthorInterface } from "./App.vue";
+
+const { author, updateBirthday } = inject<
+  Readonly<{
+    author: AuthorInterface;
+    updateBirthday: () => void;
+  }>
+>("author")!;
+
+setTimeout(() => {
+  updateBirthday();
+}, 2000);
+
+const api = inject("API_URL");
+</script>
+
+<style scoped lang="scss"></style>
