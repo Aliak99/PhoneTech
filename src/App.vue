0a5288fb8ddf5885910f697c3e7fd60a1314800f
Clément <clement@cantoo.co>
2023-03-24 18:51:32 +0100

    feat(chapitre11): :fire: Ajout dynamique de champs

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,116 @@
 <template>
-  <form @submit="submit">
+  <form>
     <div>
-      <select v-model="cityValue">
-        <option value disabled>--Choisissez une ville--</option>
-        <option value="Nice">Nice</option>
-        <option value="Paris">Paris</option>
-        <option value="Lyon">Lyon</option>
-      </select>
+      <fieldset class="InputGroup" v-for="(field, idx) in fields" :key="field.key">
+        <legend>Utilisateur #{{ idx }}</legend>
+        <label :for="`name_${idx}`">Prénom</label>
+        <Field
+          :id="`name_${idx}`"
+          :name="`users[${idx}].name`"
+          v-slot="{ field, errorMessage }"
+        >
+          <input v-bind="field" type="text" />
+          <p>{{ errorMessage }}</p>
+        </Field>
+        <p>{{ errorMessage }}</p>
+
+        <label :for="`email_${idx}`">Email</label>
+        <Field
+          :id="`email_${idx}`"
+          :name="`users[${idx}].email`"
+          type="email"
+          v-slot="{ field, errorMessage }"
+        >
+          <input v-bind="field" type="text" />
+          <p>{{ errorMessage }}</p>
+        </Field>
+        <button type="button" @click="remove(idx)">X</button>
+      </fieldset>
     </div>
-    <pre>{{ errorMessage }}</pre>
-    <div>
-      <input v-model="zipValue" type="number" />
-    </div>
-    <div>
-      <input v-model="minValue" type="number" />
-    </div>
-    <div>
-      <input v-model="maxValue" type="number" />
-    </div>
+
+    <button type="button" @click="push({ email: '', name: '' })">
+      Ajouter un utilisateur +
+    </button>
+    <br />
+    <br />
+
     <pre>{{ values }}</pre>
-    <button>Envoi</button>
+    <pre>{{ errors }}</pre>
   </form>
 </template>
 
 <script setup lang="ts">
-import { useField, useForm } from "vee-validate";
+import { useForm, useFieldArray, Field } from "vee-validate";
 import { z } from "zod";
-import { toFieldValidator } from "@vee-validate/zod";
+import { toFormValidator } from "@vee-validate/zod";
 
-const { handleSubmit, values } = useForm();
-
-const submit = handleSubmit(() => {});
-
-const { value: cityValue, errorMessage } = useField(
-  "address.city",
-  toFieldValidator(z.string({ required_error: "Veuillez choisir une ville" }), {
-    validateOnValueUpdate: false,
-    initialValue: "",
+const validationSchema = toFormValidator(
+  z.object({
+    users: z.array(
+      z.object({
+        name: z.string().min(1, { message: "Champ obligatoire" }),
+        email: z.string().email({ message: "Email invalide" }),
+      })
+    ),
   })
 );
-const { value: zipValue } = useField("address.zip.code");
 
-const { value: minValue } = useField("minMax[0]");
-const { value: maxValue } = useField("minMax[1]");
+const { values, errors } = useForm({
+  validationSchema,
+  initialValues: {
+    users: [{ name: "", email: "" }],
+  },
+});
+const { fields, push, remove } = useFieldArray("users");
 </script>
 
-<style scoped lang="scss"></style>
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  max-width: 500px;
+}
+
+input {
+  display: block;
+}
+
+span {
+  display: block;
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  margin-top: 20px;
+}
+
+button {
+  display: block;
+}
+
+button[type="submit"] {
+  margin-top: 10px;
+}
+
+form {
+  padding: 20px;
+  border: 1px solid black;
+}
+
+form + form {
+  margin-top: 20px;
+}
+
+.InputGroup {
+  padding: 10px;
+  border: 2px dotted black;
+  margin-bottom: 30px;
+  position: relative;
+}
+
+.InputGroup button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+</style>
