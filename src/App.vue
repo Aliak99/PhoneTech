ba87fee19ac47d03b20e4e4b3b4b837a6f260817
Clément <clement@cantoo.co>
2023-03-31 12:46:36 +0200

    feat(chapitre13): :fire: Introduction aux composants natifs

--- src/App.vue
+++ src/App.vue
@@ -1,139 +1,26 @@
 <template>
-  <div class="container">
-    <div class="p-20">
-      <h3 class="mb-10">Formulaire</h3>
-      <form @submit="mySubmit">
-        <input
-          ref="name"
-          v-model="nameValue"
-          class="mr-10"
-          type="text"
-          placeholder="Prénom"
-        />
-        <input v-model="emailValue" class="mr-10" type="text" placeholder="Email" />
-        <button class="btn btn-primary">Sauvegarder</button>
-      </form>
-    </div>
-    <div class="p-20">
-      <h3 class="mb-10">Liste des utilisateurs :</h3>
-      <ul>
-        <li
-          @click="state.selectedUser = user"
-          v-for="user in state.users"
-          :key="user._id"
-        >
-          <p class="mr-10 flex-fill">Name: {{ user.name }}</p>
-          <p class="mr-10 flex-fill">Email: {{ user.email }}</p>
-          <p class="mr-10">------------------------</p>
-          <button @click.stop="deleteUser(user._id)" type="button" class="btn btn-danger">
-            Supprimer
-          </button>
-        </li>
-      </ul>
-    </div>
+  <div class="p-20">
+    <button @click="selectedComponant = 'PageA'" class="btn btn-danger mr-10">
+      Page A
+    </button>
+    <button @click="selectedComponant = 'PageB'" class="btn btn-primary mr-10">
+      Page B
+    </button>
+    <Component :is="composants[selectedComponant]" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { useForm, useField } from "vee-validate";
-import { onMounted, reactive, ref, watch } from "vue";
+import PageA from './PageA.vue';
+import PageB from './PageB.vue';
+import { ref, type Component as C } from 'vue';
 
-interface User {
-  name: string;
-  email: string;
-  createdAt?: string;
-  _id?: string;
-}
+const composants: { [s:string]: C } = {
+  PageA,
+  PageB,
+};
 
-const state = reactive<{
-  users: User[];
-  selectedUser: User | null;
-}>({
-  users: [],
-  selectedUser: null,
-});
-const name = ref<HTMLInputElement | null>(null);
-
-onMounted(() => name.value?.focus());
-
-const { handleSubmit, resetForm } = useForm();
-
-const mySubmit = handleSubmit(async (value) => {
-  try {
-    if (state.selectedUser) {
-      const response = await fetch(
-        `https://restapi.fr/api/vueusers?id=${state.selectedUser._id}`,
-        {
-          method: "PATCH",
-          body: JSON.stringify(value),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const user: User = await response.json();
-      state.users = state.users.map((u) => (u._id === user._id ? user : u));
-      state.selectedUser = null;
-    } else {
-      const response = await fetch("https://restapi.fr/api/vueusers", {
-        method: "POST",
-        body: JSON.stringify(value),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const user: User = await response.json();
-      state.users.push(user);
-    }
-    resetForm();
-    name.value?.focus();
-  } catch (err) {
-    console.error(err);
-  }
-});
-
-const { value: emailValue } = useField("email");
-const { value: nameValue } = useField("name");
-
-async function fetchUsers() {
-  try {
-    const response = await fetch("https://restapi.fr/api/vueusers");
-    const users: User | User[] = await response.json();
-    if (users) {
-      state.users = Array.isArray(users) ? users : [users];
-    }
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-fetchUsers();
-
-async function deleteUser(userId?: string) {
-  try {
-    if (userId) {
-      await fetch(`https://restapi.fr/api/vueusers?id=${userId}`, {
-        method: "DELETE",
-      });
-      state.users = state.users.filter((user) => user._id !== userId);
-    }
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-watch(
-  () => state.selectedUser,
-  (user: User | null) => {
-    if (user) {
-      nameValue.value = user.name;
-      emailValue.value = user.email;
-    } else {
-      nameValue.value = "";
-      emailValue.value = "";
-    }
-  }
-);
+const selectedComponant = ref('PageA');
 </script>
 
 <style lang="scss">
