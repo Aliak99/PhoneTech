27317fb35a40f0923831273438f00e0938779335
Clément <clement@cantoo.co>
2023-03-31 17:16:02 +0200

    feat(chapitre13): :fire: Le composant Suspense

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <Page />
+  <h2>{{ evenement }}</h2>
+  <Suspense
+    @pending="myEvent('PENDING')"
+    @fallback="myEvent('FALLBACK')"
+    @resolve="myEvent('RESOLVE')"
+  >
+    <LazyList />
+    <template #fallback>
+      <h1>Chargement...</h1>
+    </template>
+  </Suspense>
 </template>
 
 <script setup lang="ts">
-import Page from "./Page.vue";
+import { defineAsyncComponent, ref } from "vue";
+
+const evenement = ref("");
+const LazyList = defineAsyncComponent(() => import("./Liste.vue"));
+
+function myEvent(eventName: string) {
+  console.log(eventName);
+  evenement.value = eventName;
+}
 </script>
 
-<style lang="scss">
-@import "./assets/base.scss";
-</style>
+<style lang="scss"></style>
