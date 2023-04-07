49638b880a231c254aa9bddf6e75c85e4a9a3065
Clément <clement@cantoo.co>
2023-04-07 12:59:35 +0200

    feat(Chapitre16): :fire: Les bases des transitions

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
-  <h2>{{ evenement }}</h2>
-  <Suspense
-    @pending="myEvent('PENDING')"
-    @fallback="myEvent('FALLBACK')"
-    @resolve="myEvent('RESOLVE')"
-  >
-    <LazyList />
-    <template #fallback>
-      <h1>Chargement...</h1>
-    </template>
-  </Suspense>
+  <div class="p-20">
+    <button class="btn btn-primary" @click="show = !show">Cliquez !</button>
+    <Transition name="mon-animation" appear>
+      <p v-if="show">
+        Principes fertur et exitiale aliquando oblato factitarunt quod propositum quod
+        illo ob illo numquam obstinatum obstinatum quoque aetatis revocari iussisse vitium
+        factitarunt inexorabiles aetatis quod accendente fertur aetatis iussisse vitium
+        similia iussisse factitarunt propositum similia neminem revocari adulatorum
+        accendente fertur ob adulatorum principes accendente inexorabiles elogio vitium
+        exitiale aliis progressu eius eius eius et non quoque vel in in quod more de
+        propositum quod oblato factitarunt poenae hoc principes exitiale cohorte hoc
+        similia hoc aliis quod de in in addictum eius adulatorum quoque et ob oblato
+        neminem numquam adulatorum addictum ob quod cohorte aliis neminem vel revocari hoc
+        hoc similia.
+      </p>
+    </Transition>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent, ref } from "vue";
+import { ref } from "vue";
 
-const evenement = ref("");
-const LazyList = defineAsyncComponent(() => import("./Liste.vue"));
-
-function myEvent(eventName: string) {
-  console.log(eventName);
-  evenement.value = eventName;
-}
+const show = ref(true);
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "./assets/scss/base.scss";
+
+@keyframes mon-animation {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(0.5, 1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+.mon-animation-enter-active {
+  animation: mon-animation 0.5s;
+}
+.mon-animation-leave-active {
+  animation: mon-animation 0.5s reverse;
+}
+</style>
